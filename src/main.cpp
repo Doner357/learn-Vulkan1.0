33354b22e5edbffe51d74b7460f1b0db12327893
Doner <don468sam@gmail.com>
2024-09-02 17:04:51 +0800

    Integrating GLFW.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,61 @@
-#include <vulkan/vulkan.h>
+// GLFW will include its own definitions and automatically
+// load the Vulkan header with it.
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+// Window's width and height
+const uint32_t WIDTH  = 800;
+const uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
     public:
         void run() {
+            initWindow();
             initVulkan();
             mainLoop();
             cleanup();
         }
 
     private:
+        // Members
+        GLFWwindow* window;
+
+        // Initialize GLFW window
+        void initWindow() {
+            // Initialize GLFW library
+            glfwInit();
+            // Hint GLFW not to create an OpenGL context
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+            // Tell GLFW window the window is unresizable
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+            // Create the actuall window
+            window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+        }
+
         void initVulkan() {
 
         }
 
         void mainLoop() {
-
+            // The application will run until either and error occrus or the
+            // window is closed.
+            while (!glfwWindowShouldClose(window)) {
+                glfwPollEvents();
+            }
+            
         }
 
         void cleanup() {
+            // Clean up GLFW resources
+            glfwDestroyWindow(window);
 
+            // Terminate GLFW
+            glfwTerminate();
         }
 };
 
