cd05f469bea61a6530027c5208adbbf217fd3884
Doner <don468sam@gmail.com>
2024-09-14 21:48:43 +0800

    Fix wrong attribute.

--- src/main.cpp
+++ src/main.cpp
@@ -221,12 +221,12 @@ class HelloTriangleApplication {
 
             // Optional, provide appication info may optimize the program.
             VkApplicationInfo app_info{};
-            app_info.sType            = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-            app_info.pApplicationName = "Hello Triangle";
-            app_info.apiVersion       = VK_MAKE_VERSION(1, 0, 0);
-            app_info.pEngineName      = "No Engine";
-            app_info.engineVersion    = VK_MAKE_VERSION(1, 0, 0);
-            app_info.apiVersion       = VK_API_VERSION_1_0;
+            app_info.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+            app_info.pApplicationName   = "Hello Triangle";
+            app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+            app_info.pEngineName        = "No Engine";
+            app_info.engineVersion      = VK_MAKE_VERSION(1, 0, 0);
+            app_info.apiVersion         = VK_API_VERSION_1_0;
 
             // Necessary, tells the Vulkan drive which global extensions and
             // validation layers want to use.
