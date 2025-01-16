1d4d71173027b027b157a538ff912b8ce46fa53a
Doner <don468sam@gmail.com>
2025-01-16 21:00:31 +0800

    Load Viking Room model.

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,13 @@
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/hash.hpp>
 #include <stb/stb_image.h>
+#include <tinyobj/tiny_obj_loader.h>
 
 
 #include <iostream>
@@ -25,12 +28,17 @@
 #include <fstream>
 #include <array>
 #include <chrono>
+#include <unordered_map>
 
 #include "headers/camera.hpp"
 
 // Window's width and height
 const uint32_t WIDTH  = 800;
 const uint32_t HEIGHT = 600;
+
+// Models path
+const std::string MODEL_PATH = "models/viking_room/viking_room.obj";
+const std::string TEXTRUE_PATH = "models/viking_room/viking_room.png";
 
 // How many in-flight frames
 const int MAX_FRAMES_IN_FLIGHT = 2;
@@ -133,8 +141,23 @@ struct Vertex {
 
         return attribute_descriptions;
     }
+
+    bool operator==(const Vertex& other) const {
+        return pos == other.pos && color == other.color && tex_coord == other.tex_coord;
+    }
 };
 
+// Custom hash function for Vertex struct
+namespace std {
+    template<>
+    struct hash<Vertex> {
+        size_t operator()(Vertex const& vertex) const {
+            return ((hash<glm::vec3>()(vertex.pos) ^
+                    (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^
+                    (hash<glm::vec2>()(vertex.tex_coord) << 1);
+        }
+    };
+}
 
 // Structure for model-view-projection matrix
 struct UniformBufferObject {
@@ -206,28 +229,11 @@ class HelloTriangleApplication {
         // Vertices for rectangle
         VkBuffer vertex_buffer;
         VkDeviceMemory vertex_buffer_memory;
-        const std::vector<Vertex> vertices = {
-            // First Square
-            // Position     // Color
-            {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},   // Upper left
-            {{ 0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 1.0f}},   // Upper right
-            {{ 0.5f,  0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 0.0f}},   // Bottom right
-            {{-0.5f,  0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f}},   // Bottom left
-
-            // Second Square
-            // Position     // Color
-            {{-0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},   // Upper left
-            {{ 0.5f, -0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {1.0f, 1.0f}},   // Upper right
-            {{ 0.5f,  0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {1.0f, 0.0f}},   // Bottom right
-            {{-0.5f,  0.5f, -0.5f}, {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f}}    // Bottom left
-        };
+        std::vector<Vertex> vertices;
         // Indices for rectangle
         VkBuffer index_buffer;
         VkDeviceMemory index_memory;
-        const std::vector<uint16_t> indices {
-            0, 1, 2, 2, 3, 0,
-            4, 5, 6, 6, 7, 4
-        };
+        std::vector<uint32_t> indices;
 
         // Uniform buffer for transformation matrix
         // Use multiple buffer to prevent the in-flight frame read the
@@ -362,6 +368,7 @@ class HelloTriangleApplication {
             createTextureImage();
             createTextureImageView();
             createTextureSampler();
+            loadModel();
             createVertexBuffer();
             createIndexBuffer();
             createUniformBuffers();
@@ -1493,7 +1500,7 @@ class HelloTriangleApplication {
         }
 
         bool hasStencilComponent(VkFormat format) {
-            return format == VK_FORMAT_D24_UNORM_S8_UINT || VK_FORMAT_D32_SFLOAT_S8_UINT;
+            return format == VK_FORMAT_D24_UNORM_S8_UINT || format == VK_FORMAT_D32_SFLOAT_S8_UINT;
         }
 
 
@@ -1502,7 +1509,7 @@ class HelloTriangleApplication {
         //////////////////////////////////////////////////////////////////
         void createTextureImage() {
             int tex_width = 0, tex_height = 0, tex_channels = 0;
-            stbi_uc* pixels = stbi_load("textures/texture.jpg", &tex_width, &tex_height, &tex_channels, STBI_rgb_alpha);
+            stbi_uc* pixels = stbi_load(TEXTRUE_PATH.c_str(), &tex_width, &tex_height, &tex_channels, STBI_rgb_alpha);
             VkDeviceSize image_size = tex_width * tex_height * 4;
 
             if (!pixels) {
@@ -1758,6 +1765,50 @@ class HelloTriangleApplication {
         }
 
 
+
+        //////////////////////////////////////////////////////////////////
+        // Vertex buffer
+        //////////////////////////////////////////////////////////////////
+        void loadModel() {
+            tinyobj::attrib_t attrib;
+            std::vector<tinyobj::shape_t> shapes;
+            std::vector<tinyobj::material_t> materials;
+            std::string warn, err;
+
+            if (!tinyobj::LoadObj(&attrib, & shapes, &materials, &warn, &err, MODEL_PATH.c_str())) {
+                throw std::runtime_error(warn + err);
+            }
+
+            std::unordered_map<Vertex, uint32_t> unique_vertices{};
+
+            for (const auto& shape : shapes) {
+                for (const auto& index : shape.mesh.indices) {
+                    Vertex vertex{};
+
+                    vertex.pos = {
+                        attrib.vertices[3 * index.vertex_index + 0],
+                        attrib.vertices[3 * index.vertex_index + 1],
+                        attrib.vertices[3 * index.vertex_index + 2]
+                    };
+
+                    vertex.tex_coord = {
+                        attrib.texcoords[2 * index.texcoord_index + 0],
+                        1.0f - attrib.texcoords[2 * index.texcoord_index + 1],
+                    };
+
+                    vertex.color = { 1.0f, 1.0f, 1.0f };
+
+                    if (unique_vertices.count(vertex) == 0) {
+                        unique_vertices[vertex] = static_cast<uint32_t>(vertices.size());
+                        vertices.push_back(vertex);
+                    }
+                    indices.push_back(unique_vertices[vertex]);
+                }
+            }
+        }
+
+
+
         //////////////////////////////////////////////////////////////////
         // Vertex buffer
         //////////////////////////////////////////////////////////////////
@@ -1994,7 +2045,7 @@ class HelloTriangleApplication {
             VkDeviceSize offsets[] = {0};
             vkCmdBindVertexBuffers(command_buffer, 0, 1, vertex_buffers, offsets);
             // Bind the index buffer, only one index buffer is allowed
-            vkCmdBindIndexBuffer(command_buffer, index_buffer, 0, VK_INDEX_TYPE_UINT16);
+            vkCmdBindIndexBuffer(command_buffer, index_buffer, 0, VK_INDEX_TYPE_UINT32);
 
             VkViewport viewport{};
             viewport.x        = 0.0f;
@@ -2203,6 +2254,7 @@ class HelloTriangleApplication {
             
             UniformBufferObject ubo{};
             ubo.model = glm::mat4(1.0f);
+            ubo.model = glm::rotate(ubo.model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
             ubo.view = camera.getViewMat();
             ubo.proj = camera.getProjectionMat();
             //ubo.proj[1][1] *= -1;   // Flip the y axis since the y axis point down in Vulkan.
